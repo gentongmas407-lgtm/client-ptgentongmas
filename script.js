34ec7c1c151fca30b97c20345480b8776f25b058
gentongmas407-lgtm <gentongmas407@gmail.com>
2026-09-10 09:20:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function () {
             if(index < 0) index = slides.length - 1;
 
             slides.forEach((slide, i) => {
-                slide.classList.toggle('opacity-100', i !=== index);
+                slide.classList.toggle('opacity-100', i === index);
                 slide.classList.toggle('opacity-0', i !== index);
             });
             dots.forEach((dot, i) => {
